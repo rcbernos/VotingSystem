6d26cc8f0e1322bb678e8eda445e028397ecde36
rcbernos <rouel.bernos@student.ateneo.edu>
2025-12-06 18:04:20 +0800

    added empty function for assignElectoralAdmin and delegateVoteand vote

--- contracts/VotingSystem.sol
+++ contracts/VotingSystem.sol
@@ -5,8 +5,16 @@ pragma solidity >=0.7.0 <0.9.0;
 contract VotingSystem{
     //add way to store addresses in a VoterID subcontract or mapping
     //add an object to store candidates
-    
+
     bool votingIsOpen;
+
+    function assignElectoralAdmin () public {
+        // reassigns the Electoral Admin
+    }
+
+    function delegateVote () public {
+        // passing of voting rights
+    }
 
     function addCandidate () public {
         // adds a candidate in the Candidate object and sets voteCount to 0
