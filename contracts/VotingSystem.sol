9177c5f7bbaa16e3266e2298968b62227e265f1b
Goodymind <aabuke9@gmail.com>
2025-12-06 20:58:15 +0800

    added owner modifier and restriction

--- contracts/VotingSystem.sol
+++ contracts/VotingSystem.sol
@@ -13,20 +13,24 @@ contract electoralAdmin{
         candidateContract = Candidate(candidate_address);
         voterContract = VoterID(voter_address);
     }
+    modifier onlyOwner() {
+        require(owner==msg.sender, "Only Admin can make changes!");
+        _;
+    }
 
-    function addCandidate(string memory newCandidate) private {
+    function addCandidate(string memory newCandidate) private onlyOwner() {
         candidateContract.addCandidate(newCandidate);
     }
 
-    function assignElectoralAdmin () public {
-        // reassigns the Electoral Admin
+    function assignElectoralAdmin (address new_admin) public onlyOwner() {
+        owner = new_admin;
     }
 
-    function openVoting() public {
+    function openVoting() public onlyOwner() {
         votingIsOpen = true;
     }
 
-    function closeVoting() public {
+    function closeVoting() public onlyOwner() {
         // prevents any additional changes to votes and VoteCount
         votingIsOpen = false;
     }
