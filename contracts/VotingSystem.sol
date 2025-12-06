7aeaa1bbf6d46339e05521364bfde7f7751d2e06
rcbernos <rouel.bernos@student.ateneo.edu>
2025-12-06 18:20:51 +0800

    created subcontracts to organize functions

--- contracts/VotingSystem.sol
+++ contracts/VotingSystem.sol
@@ -1,31 +1,11 @@
 // SPDX-License-Identifier: GPL-3.0
 
 pragma solidity >=0.7.0 <0.9.0;
-
-contract VotingSystem{
-    //add way to store addresses in a VoterID subcontract or mapping
-    //add an object to store candidates
-
+contract electoralAdmin{
     bool votingIsOpen;
 
     function assignElectoralAdmin () public {
         // reassigns the Electoral Admin
-    }
-
-    function delegateVote () public {
-        // passing of voting rights
-    }
-
-    function addCandidate () public {
-        // adds a candidate in the Candidate object and sets voteCount to 0
-    }
-
-    function resetVotes () public {
-        //reset Votes in all addresses in VoterID
-    }
-
-    function resetVoteCount() public {
-        //reset VoteCount in all Candidate entities/structs
     }
 
     function openVoting() public {
@@ -35,19 +15,43 @@ contract VotingSystem{
     function closeVoting() public {
         // prevents any additional changes to votes and VoteCount
     }
+
     function countVotes() public {
         // counts the votes for each candidate
-    }
-
-    function voteCandidate() public {
-        //select which candidate to vote and increase their vote by one. 
-        // Make sure the address has a voterID that matches to it and
-        // also has at least one vote
-        // also decrease votes in VoterID for the respective address
     }
 
     function winnerList () public {
         // show list of winners
     }
     
+}
+
+contract Candidate {
+    //add an object to store candidates
+    function addCandidate () public {
+        // adds a candidate in the Candidate object and sets voteCount to 0
+    }
+
+    function resetVoteCount() public {
+        //reset VoteCount in all Candidate entities/structs
+    }
+    
+    function receiveVote() public {
+        // add 1 voteCount to the candidate
+    }
+}
+contract VoterID {
+    //add way to store addresses in a VoterID subcontract or mapping
+    function sendVote () public {
+        // decrease 1 vote to the voter
+        // calls receiveVote() in candidate
+    }
+
+    function resetVotes () public {
+        //reset Votes in all addresses in VoterID
+    }
+
+    function delegateVote () public {
+        // passing of voting rights
+    }
 }
