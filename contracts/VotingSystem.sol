d07d51b6895aa95bbfac2841c84abf991a226336
rcbernos <rouel.bernos@student.ateneo.edu>
2025-12-06 17:56:40 +0800

    created one file for VotingSystem with planned functions and commented functionality

---
+++ contracts/VotingSystem.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract VotingSystem{
+    //add way to store addresses in a VoterID subcontract or mapping
+    //add an object to store candidates
+    
+    bool votingIsOpen;
+
+    function addCandidate () public {
+        // adds a candidate in the Candidate object and sets voteCount to 0
+    }
+
+    function resetVotes () public {
+        //reset Votes in all addresses in VoterID
+    }
+
+    function resetVoteCount() public {
+        //reset VoteCount in all Candidate entities/structs
+    }
+
+    function openVoting() public {
+        //allows voters to vote 
+    }
+
+    function closeVoting() public {
+        // prevents any additional changes to votes and VoteCount
+    }
+    function countVotes() public {
+        // counts the votes for each candidate
+    }
+
+    function voteCandidate() public {
+        //select which candidate to vote and increase their vote by one. 
+        // Make sure the address has a voterID that matches to it and
+        // also has at least one vote
+        // also decrease votes in VoterID for the respective address
+    }
+    
+}
